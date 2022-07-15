91b696fbfa45b8a4211569bdcc68344fdd41491f
Sophlanz <sophialanzendorf@gmail.com>
2022-07-15 16:24:06 +0200

    cleaned up code

--- index.js
+++ index.js
@@ -15,22 +15,20 @@ function stickyNav() {
     };
 //Open tabs from main page
 function openTab(event,tabName) {
-        
-        const tabcontent = document.getElementsByClassName('tabcontent');
-        //hide non-selected tabs
-        for (let i=0; i<tabcontent.length;i++) {
-            tabcontent[i].style.display = "none";
-        }
-        //monitor which tab is showing by removing the active classes of the links
-        const tablinks = document.getElementsByClassName('tablinksactive');
-        for (let i=0; i<tablinks.length;i++) {
-            tablinks[i].className = tablinks[i].className.replace("active", "");
-        }
-        //show the current tap
-       document.getElementById(tabName).style.display = "block";
-        //add an active class to it's tab link
-        event.currentTarget.className += "active";
-        
+    const tabcontent = document.getElementsByClassName('tabcontent');
+    //hide non-selected tabs
+    for (let i=0; i<tabcontent.length;i++) {
+        tabcontent[i].style.display = "none";
+    }
+    //monitor which tab is showing by removing the active classes of the links
+    const tablinks = document.getElementsByClassName('tablinksactive');
+    for (let i=0; i<tablinks.length;i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    //show the current tap
+    document.getElementById(tabName).style.display = "block";
+    //add an active class to it's tab link
+    event.currentTarget.className += "active";
     };
 document.getElementById("title").click();
 //Jump in the FAQ page the various sections
@@ -38,17 +36,17 @@ function jump(explanation) {
     const value =  document.getElementById(explanation);
     const yOffset = -80;
     const y = value.getBoundingClientRect().top + window.pageYOffset + yOffset;
-   
-   window.scrollTo({top:y, behavior:"smooth"});
+    window.scrollTo({top:y, behavior:"smooth"});
 };
 //tab between the menus
-function openMenu(event, desiredMenu){
+function openMenu( event, desiredMenu){
     //get the menu options content
     const menuOptions = document.getElementsByClassName('menuOptions');
     for(let i=0; i<menuOptions.length;i++) {
         menuOptions[i].style.display = "none";
-    }
-    //look to see if the active class has been added, and remove it
+    };
+    //remove active classes
+    //look to see if the active class shas been added, and remove it
     const active = document.getElementsByClassName('menuLinksactive');
     for(let i=0; i<active.length;i++){
         active[i].className = active[i].className.replace('active', '');
@@ -57,19 +55,15 @@ function openMenu(event, desiredMenu){
     document.getElementById(desiredMenu).style.display = "block";
     //add the active class
     event.currentTarget.className += "active";
-
-}
-document.getElementById('brunchMenu').click();
-
+};
 //Modal for reservation
 function openModal (){
     const modal = document.getElementById('modalReservations');
    //open the modal
    modal.style.display= "block";
-}
+};
 function closeModal(){
     const modal = document.getElementById('modalReservations');
     modal.style.display= "none"
-    
-}
-openMenu(event,"brunchMenu")();
+};
+document.getElementById('brunchMenu').click();
